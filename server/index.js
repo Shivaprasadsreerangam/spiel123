63bb17dda6e90a2df55ce4f9189332e015ad12d4
Shiva <89537710+Shivaprasadsreerangam@users.noreply.github.com>
2021-09-22 18:31:31 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -75,8 +75,10 @@ app.post("/doctordetails", (req, res) => {
   });
   app.post("/disorderdetails", (req, res) => {
     const hospital_id=req.body.hospital_id;
+    const user_id=req.body.user_id;
    
-    db.query("select distinct disorder from spiel.disorder_details_assocation where hospital_id=?;",[hospital_id], (err, result) => {
+    db.query("select dd.disorder_id,dd.disorder from spiel.disorder_details_assocation da,spiel.disorder_details dd\
+    where dd.disorder_id=da.disorder_id and da.hospital_id=1 and da.customer_id=1;;",[hospital_id,user_id], (err, result) => {
       if (err) {
         console.log(err);
       } else {
@@ -94,7 +96,7 @@ app.post("/doctordetails", (req, res) => {
     
     // // where lower_age_months <= TIMESTAMPDIFF(MONTH, ?, CURDATE())\
     // // and upper_age_months >=TIMESTAMPDIFF(MONTH, ?, CURDATE()) \;"
-    db.query("  select distinct sounds,lower_age_months,upper_age_months from spiel.articulation_sounds_ages;" ,[dirthdayDate,dirthdayDate], (err, result) => {
+    db.query("  select distinct sounds,lower_age_months,upper_age_months,sound_type,''as selected from spiel.articulation_sounds_ages  order by sounds;" ,[dirthdayDate,dirthdayDate], (err, result) => {
       if (err) {
         console.log(err);
       } else {
@@ -153,14 +155,17 @@ app.post("/doctordetails", (req, res) => {
     const Suspected_Diagnosis=req.body.Suspected_Diagnosis;
     const soundsDetails=Object.values(JSON.parse(JSON.stringify(req.body.soundsDetails)));
     const PatientSoundDisorderDetails=req.body.PatientSoundDisorderDetails;
-    
-    console.log("Yes");
-    console.log(errors_noticed);
+    const Substitution_processes=req.body.Substitution_processes;
+    const Syllable_Structure=req.body.Syllable_Structure;
+    const assimilation_processes=req.body.assimilation_processes;
+   
+    console.log(Syllable_Structure);
   
-const callProceudure="call spiel.insert_patient_articulation_details(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+const callProceudure="call spiel.insert_patient_articulation_details(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
 db.query(callProceudure,[patient_id,first_name,middle_name,last_name,Gender,disorder,birthday,Primary_Complaint,user_name,middleName,LastName,RelationShip,
 email,contact_no,'','',doctor,hospital_id,deformity_articulation,deformity,oral_mechanism,oral_mechanism,articulation_assessed,articulation_assessed_1,
-elicit_response,elicit_response,errors_noticed,errors_pattern,errors_pattern_1,Confirmed_Diagnosis,Suspected_Diagnosis,'',PatientSoundDisorderDetails
+elicit_response,elicit_response,errors_noticed,errors_pattern,errors_pattern_1,Confirmed_Diagnosis,Suspected_Diagnosis,'',PatientSoundDisorderDetails,
+Substitution_processes,Syllable_Structure,assimilation_processes
 ],(err,result)=>{
        res.send(err);
        console.log(err);
@@ -218,7 +223,7 @@ app.post("/patientdetails", (req, res) => {
   const hospital_id=req.body.hospital_id;
   const patient_id=req.body.patient_id;
   console.log(hospital_id);
-    db.query("select patient_id, patient_fname,patient_disorder,disorder_id,DATE_FORMAT(patient_dob,'%Y-%m-%d') patient_dob,Caretaker_fname,email_id,contact_number1 from spiel.patient_details where hospital_id ='?';",[hospital_id], (err, result) => {
+    db.query("select patient_id, patient_fname,patient_disorder,disorder_id,DATE_FORMAT(patient_dob,'%Y-%m-%d') patient_dob,Caretaker_fname,email_id,contact_number1 from spiel.patient_details where hospital_id ='?' and active is NULL or active='Y';",[hospital_id], (err, result) => {
       if (err) {
         console.log(err);
       } else {
@@ -231,11 +236,12 @@ app.post("/patientdetails", (req, res) => {
 
   
 app.post("/patientdetails_1", (req, res) => {
+  console.log("details")
   const hospital_id=req.body.hospital_id;
   const patient_id=req.body.patient_id;
   console.log("patientdetails_1");
-  console.log(hospital_id);
-    db.query("select patient_id, patient_fname,patient_mName,patient_lName,patient_gender,patient_disorder,disorder_id,DATE_FORMAT(patient_dob,'%Y-%m-%d') patient_dob,parimary_complain,Caretaker_fname,Caretaker_mname,Caretaker_lname,realationship,email_id,contact_number1 from spiel.patient_details where hospital_id ='?' and patient_id =?;",[hospital_id,patient_id], (err, result) => {
+  console.log(patient_id);
+    db.query("select patient_id, patient_fname,patient_mName,patient_lName,patient_gender,patient_disorder,disorder_id,substr(patient_dob,1,10) as patient_dob,parimary_complain,Caretaker_fname,Caretaker_mname,Caretaker_lname,realationship,email_id,contact_number1,doctor_assigned from spiel.patient_details where  patient_id =?;",[patient_id], (err, result) => {
       if (err) {
         console.log(err);
       } else {
@@ -243,6 +249,7 @@ app.post("/patientdetails_1", (req, res) => {
         console.log(result);
        
       }
+      console.log("patientdetails_1");
     });
   });
 
@@ -251,7 +258,7 @@ app.post("/patientdetails_1", (req, res) => {
     const patient_id=req.body.patient_id;
     console.log("patientdetails_1");
     console.log(hospital_id);
-      db.query("select disorder,deformity,deformity_list,Oral_mechanism_significant,articulation_assessed,elicit_response_method,errors_noticed,pattern_errors,diagnosis_confirm_suspect,diagnosis_severity,Diagnostic_terms from spiel.patient_articulation_disorder_details where patient_id =?",[patient_id], (err, result) => {
+      db.query("select disorder,deformity,deformity_list,Oral_mechanism_significant,articulation_assessed,elicit_response_method,errors_noticed,pattern_errors,diagnosis_confirm_suspect,diagnosis_severity,Diagnostic_terms,Substitution_processes,Syllable_Structure_processes,assimilation_processes from spiel.patient_articulation_disorder_details where patient_id =?",[patient_id], (err, result) => {
         if (err) {
           console.log(err);
         } else {
@@ -293,6 +300,23 @@ app.post("/patientdetails_1", (req, res) => {
       });
     });
 
+    app.post("/deletePatientDetails", (req, res) => {
+      const hospital_id=req.body.hospital_id;
+      const patient_id=req.body.patient_id;
+     
+  
+      console.log(hospital_id);
+        db.query("update spiel.patient_details set active='N' where Patient_id=? and hospital_id=?",[patient_id,hospital_id], (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            res.send(result);
+            console.log(result);
+           
+          }
+        });
+      });
+
     app.post("/getArticulationPatientDetails", (req, res) => {
       const patient_id=req.body.patient_id;
      
@@ -309,16 +333,100 @@ app.post("/patientdetails_1", (req, res) => {
         });
       });
 
+      
+    app.post("/getFluencyPatientDetails", (req, res) => {
+      const patient_id=req.body.patient_id;
+     
+  
+      console.log(patient_id);
+        db.query("select cause_neurogenic_disorder,family_history,contacted_stutterers,primary_features_observed,secondary_features_observed,Respiration,phonatory,articulatory,\
+        sample_speech,Percentage_syllables_stuttered,stuttering_duration_average,overall_speaking_rate,articulatory_rate,nonstuttered_average_duration,\
+        nonstuttered_longest_length,nonstuttered_longest_duration,nonstuttered_average_length,Naturalness_rating_scale,language_changes,language,code_switching_noticed,\
+        test_materials,psychological_evaluation,diagnosis_confirmed_suspected,Suspected_Diagnostic,Confirmed_Diagnosis from spiel.patient_fluency_disorder_details where patient_id =?;",
+        [patient_id] , (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            res.send(result);
+            console.log(result);
+           
+          }
+        });
+      });
+
+
+      app.post("/getChildLanguageDisorderPatientDetails", (req, res) => {
+        const patient_id=req.body.patient_id;
+        const hospital_id=req.body.hospital_id;
+       
+    
+        console.log(patient_id);
+          db.query("select language_assessment,pre_verb_skill_observation,receptive_vocabulary,expressive_vocabulary, pre_reading_skills,developmental_language,\
+          advanced_language,mother_tongue,other_lanugauge,community_lanugauge,client_assessed_tool,recepitive_tool_language,expressive_tool_language,\
+          client_assessed_disorder_specific_tool,disorder_suspected,language_delay_suspected,language_delay_secondary_disorder,consq_language_delay_secondary_disorder,diagnosis_confirmed_suspected\
+          from spiel.patient_child_language_disorder where hospital_id = ? and patient_id=?;",
+          [hospital_id,patient_id] , (err, result) => {
+            if (err) {
+              console.log(err);
+            } else {
+              res.send(result);
+              console.log(result);
+             
+            }
+          });
+        });
+
+        app.post("/getAudultLanguageDisorderPatientDetails", (req, res) => {
+          const patient_id=req.body.patient_id;
+          const hospital_id=req.body.hospital_id;
+         
+      
+          console.log(patient_id);
+            db.query("select language_assessment,significant_medical_history,morbid_change,adult_neurogenic_communication_disorder,dementia,right_hemisphere_damage,\
+            alzheimer,parkinsons,dysphagia,apraxia,test_material,test_meterial_file,significant_issue_speech,diagnosis_confirmed_suspected,\
+            from spiel.patient_adult_language_disorder where patient_id=? and hospital_id=?;)",
+            [hospital_id,patient_id] , (err, result) => {
+              if (err) {
+                console.log(err);
+              } else {
+                res.send(result);
+                console.log(result);
+               
+              }
+            });
+          });
 
 
 
 app.post("/login", (req, res) => {
     const uname=req.body.uname;
     const psw=req.body.psw;
-     const sqlFetch="select id,email_id,name,invalid_logins,hospital_id,role from spiel.spiel_user where (email_id=? or id =?) and pwd=? and user_expiry_date > current_date();"
+     const sqlFetch="select id,email_id,name,invalid_logins,hospital_id,role from spiel.spiel_user where (email_id=? or id =?) and pwd=? ;"
      db.query(sqlFetch,[uname,uname,psw],(err,result)=>{
-         res.send(result); 
-         console.log(result);
+      if (err) {
+        console.log(err);
+        res.send("Invalid user name or password");
+      } else {
+        res.send(result);
+       // console.log(result);
+       
+      }
+});
+});
+
+app.post("/userExpire", (req, res) => {
+  const uname=req.body.uname;
+  const psw=req.body.psw;
+   const sqlFetch="select id,email_id,name,invalid_logins,hospital_id,role,substr(user_expiry_date,1,10) as user_expiry_date from spiel.spiel_user where (email_id=? or id =?) and pwd=? and user_expiry_date < current_date();"
+   db.query(sqlFetch,[uname,uname,psw],(err,result)=>{
+    if (err) {
+      console.log(err);
+      res.send("Invalid user name or password");
+    } else {
+      res.send(result);
+      console.log(result);
+     
+    }
 });
 });
 
@@ -374,14 +482,7 @@ app.post("/insert", (req, res) => {
     const diagnosis_con_sus_1=req.body.diagnosis_con_sus_1;
     const Confirmed_Diagnosis=req.body.Confirmed_Diagnosis;
     const Suspected_Diagnosis=req.body.Suspected_Diagnosis;
-    const soundsDetails=Object.values(JSON.parse(JSON.stringify(req.body.soundsDetails)));
     const PatientSoundDisorderDetails=req.body.PatientSoundDisorderDetails;
-   
-
-    const stringData = req.body.soundsDetails.reduce((result, item) => {
-      return `${result}${item.lat},${item.lon}|`
-    }, "");
-
     console.log(hospital_id);
   
 const callProceudure="call spiel.insert_patient_details(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
@@ -405,7 +506,7 @@ app.post("/createAccount", (req, res) => {
   const disorder="Voice";
   const Emaild=req.body.Emaild;
   const Phno=req.body.Phno;
-  
+   
 
  
   
@@ -419,6 +520,221 @@ db.query(callProceudure,[hospital_id,uName,mName,lName,hName,duration,duration1,
 
       });
 });
+
+
+app.post("/insertFluencyPatientDetails", (req, res) => {
+  console.log("entered Index");
+  const patient_id=req.body.patient_id;
+  const first_name=req.body.first_name;
+  const middle_name=req.body.middle_name;
+  const last_name=req.body.last_name;
+  const Gender=req.body.Gender;
+  const disorder=req.body.disorder;
+  const birthday=req.body.birthday;
+  const Primary_Complaint=req.body.Primary_Complaint;
+  const user_name=req.body.user_name;
+  const middleName=req.body.middleName;
+  const LastName=req.body.LastName;
+  const RelationShip=req.body.RelationShip;
+  const email=req.body.email;
+  const contact_no=req.body.contact_no;
+  const doctor=req.body.doctor;
+  const hospital_id=req.body.hospital_id;
+  const cause_neurogenic_disorder= req.body.cause_neurogenic_disorder; 
+const family_history= req.body.family_history; 
+const contacted_stutterers= req.body.contacted_stutterers; 
+const primary_features_observed= req.body.primary_features_observed; 
+const secondary_features_observed= req.body.secondary_features_observed; 
+const Respiration= req.body.Respiration; 
+const phonatory= req.body.phonatory; 
+const articulatory= req.body.articulatory; 
+const sample_speech= req.body.sample_speech; 
+const Percentage_syllables_stuttered= req.body.Percentage_syllables_stuttered; 
+const stuttering_duration_average= req.body.stuttering_duration_average; 
+const overall_speaking_rate= req.body.overall_speaking_rate; 
+const articulatory_rate= req.body.articulatory_rate; 
+const nonstuttered_average_duration= req.body.nonstuttered_average_duration; 
+const nonstuttered_longest_length= req.body.nonstuttered_longest_length; 
+const nonstuttered_longest_duration= req.body.nonstuttered_longest_duration; 
+const nonstuttered_average_length= req.body.nonstuttered_average_length; 
+const Naturalness_rating_scale= req.body.Naturalness_rating_scale; 
+const language_changes= req.body.language_changes; 
+const language= req.body.language; 
+const code_switching_noticed= req.body.code_switching_noticed; 
+const test_materials= req.body.test_materials; 
+const psychological_evaluation= req.body.psychological_evaluation; 
+const diagnosis_confirmed_suspected= req.body.diagnosis_confirmed_suspected; 
+const Suspected_Diagnostic= req.body.Suspected_Diagnostic; 
+const Confirmed_Diagnosis =req.body.Confirmed_Diagnosis;
+
+ 
+
+const callProceudure="call spiel.insert_patient_fluency_details(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+db.query(callProceudure,[patient_id,
+  first_name,
+  middle_name,
+  last_name,
+  Gender,
+  disorder,
+  birthday,
+  Primary_Complaint,
+  user_name,
+  middleName,
+  LastName,
+  RelationShip,
+  email,
+  contact_no,
+  '',
+  '',doctor
+  ,hospital_id,
+  cause_neurogenic_disorder,
+family_history,
+contacted_stutterers,
+primary_features_observed,
+  secondary_features_observed,
+  Respiration,
+  phonatory,
+  articulatory,
+  sample_speech,
+  Percentage_syllables_stuttered,
+  stuttering_duration_average,
+  overall_speaking_rate,
+  articulatory_rate,
+  nonstuttered_average_duration,
+  nonstuttered_longest_length,
+  nonstuttered_longest_duration,
+  nonstuttered_average_length,
+  Naturalness_rating_scale,
+  language_changes,
+  language,
+  code_switching_noticed,
+  test_materials,
+  psychological_evaluation,
+  diagnosis_confirmed_suspected,
+  Suspected_Diagnostic,
+  Confirmed_Diagnosis
+],(err,result)=>{
+     res.send(err);
+     console.log(err);
+     console.log(result);
+
+    });
+});
+
+app.post("/insertLanguageDisorderPatientDetails", (req, res) => {
+  console.log("entered Index");
+  const patient_id=req.body.patient_id;
+  const first_name=req.body.first_name;
+  const middle_name=req.body.middle_name;
+  const last_name=req.body.last_name;
+  const Gender=req.body.Gender;
+  const disorder=req.body.disorder;
+  const birthday=req.body.birthday;
+  const Primary_Complaint=req.body.Primary_Complaint;
+  const user_name=req.body.user_name;
+  const middleName=req.body.middleName;
+  const LastName=req.body.LastName;
+  const RelationShip=req.body.RelationShip;
+  const email=req.body.email;
+  const contact_no=req.body.contact_no;
+  const doctor=req.body.doctor;
+  const hospital_id=req.body.hospital_id;
+  const language_assessment=req.body.language_assessment ;
+const significant_medical_history =req.body. significant_medical_history;
+const morbid_change =req.body. morbid_change;
+const adult_neurogenic_communication_disorder=req.body.adult_neurogenic_communication_disorder ;
+const dementia =req.body. dementia;
+const right_hemisphere_damage=req.body.right_hemisphere_damage ;
+const alzheimer= req.body. alzheimer;
+const parkinsons =req.body. parkinsons;
+const dysphagia=req.body.dysphagia ;
+const apraxia= req.body. apraxia;
+const test_material =req.body. test_material;
+const test_meterial_file=req.body.test_meterial_file ;
+const significant_issue_speech =req.body. significant_issue_speech;
+const pre_verb_skill_observation =req.body.pre_verb_skill_observation  ;
+const receptive_vocabulary =req.body.receptive_vocabulary  ;
+const expressive_vocabulary=req.body.expressive_vocabulary ;
+const pre_reading_skills=req.body.pre_reading_skills ;
+const developmental_language=req.body.developmental_language ;
+const advanced_language=req.body.advanced_language ;
+const mother_tongue =req.body.mother_tongue  ;
+const other_lanugauge =req.body.other_lanugauge  ;
+const community_lanugauge=req.body.community_lanugauge ;
+const client_assessed_tool=req.body.client_assessed_tool ;
+const recepitive_tool_language =req.body. recepitive_tool_language;
+const expressive_tool_language=req.body. expressive_tool_language;
+const client_assessed_disorder_specific_tool=req.body.client_assessed_disorder_specific_tool ;
+const disorder_suspected =req.body.disorder_suspected  ;
+const language_delay_suspected=req.body.language_delay_suspected ;
+const language_delay_secondary_disorder =req.body.language_delay_secondary_disorder  ;
+const consq_language_delay_secondary_disorder =req.body.consq_language_delay_secondary_disorder  ;
+const diagnosis_confirmed_suspected= req.body. diagnosis_confirmed_suspected;
+  
+
+ 
+
+const callProceudure="call spiel.insert_patient_language_disorder_details(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+db.query(callProceudure,[patient_id,
+  first_name,
+  middle_name,
+  last_name,
+  Gender,
+  disorder,
+  birthday,
+  Primary_Complaint,
+  user_name,
+  middleName,
+  LastName,
+  RelationShip,
+  email,
+  contact_no,
+  '',
+  '',
+  doctor,
+  hospital_id,
+language_assessment ,
+pre_verb_skill_observation  ,
+receptive_vocabulary  ,
+expressive_vocabulary ,
+pre_reading_skills ,
+developmental_language ,
+advanced_language ,
+mother_tongue  ,
+other_lanugauge  ,
+community_lanugauge ,
+client_assessed_tool ,
+recepitive_tool_language,
+expressive_tool_language,
+client_assessed_disorder_specific_tool ,
+disorder_suspected  ,
+language_delay_suspected ,
+language_delay_secondary_disorder  ,
+consq_language_delay_secondary_disorder  ,
+diagnosis_confirmed_suspected,
+significant_medical_history,
+morbid_change,
+adult_neurogenic_communication_disorder ,
+dementia,
+right_hemisphere_damage ,
+alzheimer,
+parkinsons,
+dysphagia ,
+apraxia,
+test_material,
+test_meterial_file ,
+significant_issue_speech
+
+ 
+],(err,result)=>{
+     res.send(err);
+     console.log(err);
+     console.log(result);
+
+    });
+});
+
+
 
 app.listen(3002,()=>{
     console.log("running port 3002");
